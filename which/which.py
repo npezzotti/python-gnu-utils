ec6e2e9a8321db9fcb7e193b02129a371b81a891
npezzotti <nathan.pezzotti@datadoghq.com>
2022-10-11 22:07:28 -0400

    cross platform, faster

--- which/which.py
+++ which/which.py
@@ -2,32 +2,37 @@ import os
 import sys
 
 
-def _find_files(name, start='.'):
+def find_files(name, start='.'):
     for relpath, _, files in os.walk(start):
         if name in files:
             path = os.path.join(relpath, name)
             yield os.path.normpath(os.path.abspath(path))
 
+def get_path_dirs():
+    path = os.getenv('PATH')
+    
+    return path.split(';') if sys.platform.startswith('win32') else path.split(':')
+
 def main(args):
-    dirs = os.getenv('PATH').split(':')
+    dirs = get_path_dirs()
 
-    results = []
+    result = ''
     for dir in dirs:
-        files = _find_files(args.program, dir)
-        for file in files:
-            results.append(file)
+        for file in find_files(args.program, dir):
+            if args.a and not args.s:
+                print(file)
+            else:
+                result = file
+                break
+        if result:
+            break
 
-    if not results:
+    if not result:
         sys.exit(1)
     
-    if args.s:
-        pass
-    elif args.a:
-        for file in results:
-            print(file)
-    else:
-        print(results[0])
-
+    if not args.s:
+        print(result)
+    
     sys.exit(0)
 
 if __name__ == '__main__':
